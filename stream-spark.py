b08d56444e81591eb210deafd5609e537ea513db
David Simon Reyes Lopez <simonchi@MacBook-Pro-de-David.local>
2025-03-08 13:54:32 -0600

    nueva_actualizacion

--- stream-spark.py
+++ stream-spark.py
@@ -3,16 +3,17 @@ import requests
 import pandas  as pd
 import json
 
-def post_spark_job(user, repo, job, token,codeurl, dataseturl):
+def post_spark_job(user, repo, job, token, codeurl, dataseturl):
     # Define the API endpoint
     url = 'https://api.github.com/repos/' + user + '/' + repo + '/dispatches'
     # Define the data to be sent in the POST request
     payload = {
       "event_type": job,
-      "client_playload": {
-          "codeurl":  codeurl,
-          "dataseturl": dataseturl
+      "client_payload": {
+      "codeurl": codeurl,
+      "dataseturl": dataseturl
       }
+
     }
 
     headers = {
@@ -31,29 +32,28 @@ def post_spark_job(user, repo, job, token,codeurl, dataseturl):
     # Display the response in the app
     st.write(response)
 
-st.title("Spark & streamlit")
-
-st.header("spark-submit Job")
-
-github_user  =  st.text_input('Github user', value='adsoftsito')
-github_repo  =  st.text_input('Github repo', value='bigdata')
-spark_job    =  st.text_input('Spark job', value='spark')
-github_token =  st.text_input('Github token', value='***')
-code_url     =  st.text_input('Code URL', value='')
-dataset_url  =  st.text_input('Dataset URL', value='')
-
-
-if st.button("POST spark submit"):
-    post_spark_job(github_user, github_repo, spark_job, github_token, code_url, dataset_url)
-
-# Funcion para obtener un get (checar en donde estan los resultados)
-
 def get_spark_results(url_results):
     response = requests.get(url_results)
     st.write(response)
 
     if  (response.status_code ==  200):
         st.write(response.json())
+
+
+
+st.title("Spark & streamlit")
+
+st.header("spark-submit Job")
+
+github_user  =  st.text_input('Github user', value='DavidSimonch')
+github_repo  =  st.text_input('Github repo', value='spark')
+spark_job    =  st.text_input('Spark job', value='spark')
+github_token =  st.text_input('Github token', value='*')
+code_url     =  st.text_input('Code URL', value='')
+dataset_url  =  st.text_input('Dataset URL', value='')
+
+if st.button("POST spark submit"):
+    post_spark_job(github_user, github_repo, spark_job, github_token, code_url, dataset_url)
 
 st.header("spark-submit results")
 
